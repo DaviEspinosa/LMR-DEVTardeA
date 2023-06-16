fa1fbb924f786a6eb6a2246834d42ab58c02a4f5
DaviEspinosa <123770867+DaviEspinosa@users.noreply.github.com>
2023-06-16 13:48:09 +0000

    ...

--- Atividade16_06/index/index.js
+++ Atividade16_06/index/index.js
@@ -1,4 +1,54 @@
-let radio = document.querySelector('.manual-btn')
+/*-----HAMBURGUER-NAV----- */
+
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+  );
+  mobileNavbar.init();
+
+
+  /*-----SLIDES----- */
+
+  let radio = document.querySelector('.manual-btn')
 let cont =  1
 
 document.getElementById('radio1').checked = true
